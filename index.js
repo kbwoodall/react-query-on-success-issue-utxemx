54b88626f3c80cb1d68748795ab564d0b1869533
kbwoodall <kerry.woodall@gmail.com>
2022-03-21 21:29:32 -0700

    1 file modified

--- index.js
+++ index.js
@@ -5,19 +5,31 @@ import { useQuery } from 'react-query';
 import Hello from './Hello';
 import './style.css';
 
+const Getstuff = () => {
+  const { data = [], status } = useQuery({
+    queryKey: 'id',
+    queryFn: () =>
+      fetch('https://kwoodallsapp.herokuapp.com/getitems').then((res) =>
+        res.json()
+      ),
+    config: {
+      onSuccess: (data) => [...data].reverse(),
+    },
+  });
+  console.log('data ' + data);
+};
+const Showdb = () => {
+  {
+    data.map(({ name, phone }, index) => (
+      <div key={index}>
+        <h3>{phone}</h3>
+        <p> {name}</p>
+      </div>
+    ));
+  }
+};
+
 const App = () => {
-  const Getstuff = () => {
-    const { data = [], status } = useQuery({
-      queryKey: 'id',
-      queryFn: () =>
-        fetch('https://kwoodallsapp.herokuapp.com/getitems').then((res) =>
-          res.json()
-        ),
-      config: {
-        onSuccess: (data) => [...data].reverse(),
-      },
-    });
-  };
   return (
     <div>
       <Hello name={'phone list'} />
